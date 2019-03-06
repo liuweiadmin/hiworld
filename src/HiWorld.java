28c67d107ae3f3fde6b5ba41c985c4a3467af734
Lenovo <1764672083@qq.com>
2019-03-06 19:01:21 +0800

    test

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,7 +1,24 @@
-public class HiWorld {
-      public static void main(String[] args) {
+import java.util.Scanner;
 
-          System.out.println("Hi World  II  ss");
+public class HiWorld {
+
+    public void method1() {
+        Scanner   sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c;
+        c = a + b;
+
+        System.out.println(c);
+
+    }
+
+
+    public static void main(String[] args) {
+          HiWorld    HW = new HiWorld();
+
+          HW.method1();
+        System.out.println("Hi World  II  ss");
 
     }
 }
